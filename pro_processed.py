bed121973e5218a767af1332455706679615ddbe
xuezhen <xuezhen1999@icloud.com>
2024-09-07 00:34:33 +0800

    重新格式化了代码，增加了数据记录的输出

--- pro_processed.py
+++ pro_processed.py
@@ -1,4 +1,5 @@
 import csv
+import warnings
 
 import mne
 import numpy as np
@@ -8,290 +9,329 @@ from mne_icalabel import label_components
 
 
 def find_folders_containing_string(directory, search_strings):
-	"""
-	遍历指定目录及其子目录，筛选出名称中包含特定字符串的所有子目录。
-
-	:param directory: 要搜索的目录路径（字符串）
-	:param search_strings: 要搜索的字符串列表（子目录名称中必须包含其中任意一个字符串）
-	:return: 包含符合条件的子目录名称的列表
-	"""
-	matching_folders = []
-	search_strings = [s.upper() for s in search_strings]  # 将所有搜索字符串转换为大写
-	a = os.walk(directory)
-	
-	# 遍历目录树
-	for root, dirs, files in os.walk(directory):
-		# 检查当前目录的名字是否包含目标字符串
-		for filename in files:
-			if filename.endswith('set'):
-				# 检查文件名是否包含任意一个搜索字符串
-				if any(s in filename.upper() for s in search_strings):
-					relative_path = os.path.join(root, filename)
-					matching_folders.append(relative_path)
-	
+	"""查找包含指定字符串的子目录。"""
+	if isinstance(search_strings, str):
+		search_strings = [search_strings]
+	search_strings = [s.upper() for s in search_strings]
+	matching_folders = [
+		os.path.join(root, filename)
+		for root, dirs, files in os.walk(directory)
+		for filename in files
+		if filename.endswith('set') and any(s in filename.upper() for s in search_strings)
+	]
 	return matching_folders
-	
 
-
-
-def insert_before_nth_from_end(original_string, insert_string, n_from_end):
-	# 计算插入位置
-	position = len(original_string) - n_from_end
-	
-	# 确保插入位置在字符串的有效范围内
-	if position < 0:
-		position = 0
-	elif position > len(original_string):
-		position = len(original_string)
-	
-	# 使用切片插入字符串
-	new_string = original_string[:position] + insert_string + original_string[position:]
-	return new_string
 
 class Per_Process:
 	def __init__(self):
 		self.record_dict = {}
 		self.if_baseline = True
-	
-	def epochs_to_raw(self, epochs):
-		# 提取 Epochs 的数据和信息
-		data = epochs.get_data()  # (n_epochs, n_channels, n_samples)
-		info = epochs.info.copy()
-		sfreq = info['sfreq']
-		tmin = epochs.tmin
-		
-		# 将 Epochs 数据重新构造成 Raw 数据
-		n_epochs, n_channels, n_samples = data.shape
-		raw_data = np.reshape(data, (n_channels, n_epochs * n_samples))
-		
-		# 创建时间轴
-		times = np.arange(n_epochs * n_samples) / sfreq + tmin
-		
-		# 创建 Raw 对象
-		raw = mne.io.RawArray(raw_data, info)
-		
-		# 设置时间轴
-		raw.times = times
-		
-		return raw
+		self.base_dir = '..\\sub_test'
+		# 'matb',
+		self.task_keywords = ('matb', 'back')
+		self.channels_drop = ['Cz']
+		self.save_folder_name = 'Per_Process_1'
+		self.eeg_freqs = (1, 100)
+		self.notch_freqs = (48, 52)
+		self.ecg_freq = (0.04, 40)
+		self.set_threshold()
+		self.if_repair_artifact()
+		self.process = ''
+		self.first_call = True
 	
 	def update_record(self, key, value):
-		"""
-		更新记录字典。如果key存在，则将value追加到该key对应的值列表中；
-		如果key不存在，则创建一个新的key，并将value作为列表中的第一个元素。
-
-		Parameters:
-		key : str
-			要更新或创建的键
-		value : any
-			要追加或设置的值
-		"""
-		if key in self.record_dict:
-			# 如果key存在，追加value到对应的值列表中
-			if isinstance(self.record_dict[key], list):
-				self.record_dict[key].append(value)
-			else:
-				# 如果存在的值不是列表，转换为列表并追加
-				self.record_dict[key] = [self.record_dict[key], value]
-		else:
-			# 如果key不存在，创建一个新的键，并将value作为列表中的第一个元素
-			self.record_dict[key] = [value]
-		
-	def save_to_csv(self):
-		file_path = 'record.csv'  # 文件名固定为 record.csv
+		"""更新记录字典。"""
+		self.record_dict.setdefault(key, []).append(value)
+	
+	def save_to_csv(self, path):
+		"""将记录字典保存为 CSV 文件。"""
+		file_path = os.path.join(path, 'record.csv')
 		with open(file_path, 'w', newline='') as file:
 			writer = csv.writer(file)
 			for key, values in self.record_dict.items():
 				writer.writerow([key] + values)
 	
+	def save_to_txt(self, path):
+		# 将属性写入到txt文件
+		file_path = os.path.join(path, 'attribute.txt')
+		with open(file_path, 'w', newline='') as file:
+			file.write(f"Threshold Drop Bad Channels: {self.threshold_drop_bad_channels}\n")
+			file.write(f"Threshold Annotate Muscle Z-Score: {self.threshold_annotate_muscle_zscore}\n")
+			file.write(f"Threshold Find Bads Muscle: {self.threshold_find_bads_muscle}\n")
+			file.write(f"Threshold Pro ICALabel: {self.threshold_pro_icalabel}\n")
+			file.write(f"If Drop Bad Channels: {self.if_drop_bad_channels}\n")
+			file.write(f"If Annotate Muscle Z-Score: {self.if_annotate_muscle_zscore}\n")
+			file.write(f"If Find Bads Muscle: {self.if_find_bads_muscle}\n")
+			file.write(f"If Find Bads ECGs: {self.if_find_bads_ecgs}\n")
+			file.write(f"If Find Bads EOGs: {self.if_find_bads_eogs}\n")
+			file.write(f"If Baseline: {self.if_baseline}\n")
+			file.write(f"Base Directory: {self.base_dir}\n")
+			file.write(f"Task Keywords: {self.task_keywords}\n")
+			file.write(f"Channels Drop: {self.channels_drop}\n")
+			file.write(f"Save Folder Name: {self.save_folder_name}\n")
+			file.write(f"EEG Frequencies: {self.eeg_freqs}\n")
+			file.write(f"Notch Frequencies: {self.notch_freqs}\n")
+			file.write(f"ECG Frequency: {self.ecg_freq}\n")
+			file.write(f"Process:{self.process}\n")
+	
+	def set_threshold(self):
+		# 默认1.5
+		self.threshold_drop_bad_channels = 1.5
+		# 默认4
+		self.threshold_annotate_muscle_zscore = 4
+		# 默认0.5
+		self.threshold_find_bads_muscle = 0.5
+		self.threshold_pro_icalabel = 0.9
+	
+	def if_repair_artifact(self):
+		self.if_drop_bad_channels = True
+		self.if_annotate_muscle_zscore = True
+		self.if_find_bads_muscle = True
+		self.if_find_bads_ecgs = True
+		self.if_find_bads_eogs = True
+	
+	def append_string(self, input_string):
+		# 拼接 self.process 和输入字符串，使用 '->' 连接
+		if self.first_call:
+			self.process = self.process+'->'+input_string
+
+	
+	def epochs_to_raw(self, epochs):
+		warnings.warn("此函数还未验证正确性", UserWarning)
+		# 提取 Epochs 的数据和信息
+		data = epochs.get_data()  # (n_epochs, n_channels, n_samples)
+		info = epochs.info.copy()
+		
+		# 将 Epochs 数据重新构造成 Raw 数据
+		n_epochs, n_channels, n_samples = data.shape
+		raw_data = np.reshape(data, (n_channels, n_epochs * n_samples))
+		# 创建 Raw 对象
+		raw = mne.io.RawArray(raw_data, info)
+		return raw
+	
 	def get_dir(self):
+		base_dir = self.base_dir
+		task_keywords = self.task_keywords
 		dir = {}
-		sub_dirs = os.listdir('../sub')
-		for sub_number in sub_dirs:
-			sub_dir = '../sub/' + sub_number
-			sessions = os.listdir(sub_dir)
-			for session in sessions:
-				sub_session_egg_dir = sub_dir + '/' + session + '/eeg'
-				sub_session_egg_task_dir_lists = find_folders_containing_string(sub_session_egg_dir, ['matb','back'])
-				for sub_session_egg_task_dir in sub_session_egg_task_dir_lists:
-					dir[sub_number + '_' + session + '_' + sub_session_egg_task_dir.split("\\")[1][:-4]] = sub_session_egg_task_dir
+		for sub_number in os.listdir(base_dir):
+			sub_dir = os.path.join(base_dir, sub_number)
+			for session in os.listdir(sub_dir):
+				eeg_dir = os.path.join(sub_dir, session, 'eeg')
+				eeg_task_dir_lists = find_folders_containing_string(eeg_dir, task_keywords)
+				for eeg_task_dir in eeg_task_dir_lists:
+					eeg_task_dir = os.path.normpath(eeg_task_dir)
+					eeg_task_dir_name = eeg_task_dir.split("\\")[-1][:-4]
+					dir[f"{sub_number}_{session}_{eeg_task_dir_name}"] = eeg_task_dir
 		return dir
 	
-	def get_data(self):
-		return self.get_dir()
-	
 	def drop_channel(self, raw):
-		if 'Cz' in raw.info['ch_names']:
-			raw.drop_channels('Cz')
+		"""注意检查通道大小写"""
+		channels = self.channels_drop
+		for channel in channels:
+			if channel in raw.info['ch_names']:
+				raw.drop_channels('Cz')
 		return raw
 	
 	def filter_data(self, raw):
-		raw.filter(l_freq=1, h_freq=100, picks='eeg')
-		raw.notch_filter(freqs=[48, 52], picks='eeg')
-		
-		raw.filter(l_freq=0.04, h_freq=40, picks='ecg')
+		eeg_freqs = self.eeg_freqs
+		notch_freqs = self.notch_freqs
+		ecg_freq = self.ecg_freq
+		raw.filter(l_freq=eeg_freqs[0], h_freq=eeg_freqs[1], picks='eeg')
+		raw.notch_filter(freqs=notch_freqs, picks='eeg')
+		raw.filter(l_freq=ecg_freq[0], h_freq=ecg_freq[1], picks='ecg')
 		return raw
 	
 	def drop_bad_channels(self, raw):
-		# 选取坏导
-		bad_channels = find_bad_channels_lof(raw, picks='eeg')
-		raw.info['bads'].extend(bad_channels)
-		raw.interpolate_bads()
-		
-		self.update_record('bad_channels', len(bad_channels))
+		if self.if_drop_bad_channels:
+			# 选取坏导
+			bad_channels = find_bad_channels_lof(raw, threshold=self.threshold_drop_bad_channels, picks='eeg')
+			raw.info['bads'].extend(bad_channels)
+			raw.interpolate_bads()
+			
+			self.update_record('bad_channels', len(bad_channels))
 		return raw
 	
 	def drop_bad_period(self, raw):
+		if self.if_annotate_muscle_zscore:
+			# 标记肌电伪迹
+			annot_muscle, scores_muscle = annotate_muscle_zscore(
+				raw,
+				ch_type='eeg',
+				filter_freq=(1, 100)
+			)
+			raw.set_annotations(annot_muscle)
+			
+			# 获取注释和时间
+			annot = raw.annotations
+			bad_intervals = [(annot.onset[i], annot.onset[i] + annot.duration[i])
+			                 for i in range(len(annot)) if annot.description[i] == 'BAD_muscle']
+			
+			# 创建掩码以排除坏段
+			mask = np.ones(len(raw.times), dtype=bool)
+			for start, end in bad_intervals:
+				mask[(raw.times >= start) & (raw.times < end)] = False
+			
+			# 过滤数据
+			data_clean = raw.get_data()[:, mask]
+			# 重新创建 Raw 对象
+			info = raw.info
+			raw_clean = mne.io.RawArray(data_clean, info)
+			# 提取注释信息
+			bad_durations = np.sum(annot.duration)
+			self.update_record('total_bad_duration', str(bad_durations))
 		
-		raw_copy = raw.copy()
-		# 标记肌电伪迹
-		annot_muscle, scores_muscle = annotate_muscle_zscore(
-			raw,
-			ch_type='eeg',
-			filter_freq=(1, 100)
-		)
-		raw.set_annotations(annot_muscle)
-
-		# 获取注释和时间
-		annot = raw.annotations
-		bad_intervals = [(annot.onset[i], annot.onset[i] + annot.duration[i])
-		                 for i in range(len(annot)) if annot.description[i] == 'BAD_muscle']
-		
-		# 创建掩码以排除坏段
-		mask = np.ones(len(raw.times), dtype=bool)
-		for start, end in bad_intervals:
-			mask[(raw.times >= start) & (raw.times < end)] = False
-		
-		# 过滤数据
-		data_clean = raw.get_data()[:, mask]
-		
-		# 重新创建 Raw 对象
-		info = raw.info
-		raw_clean = mne.io.RawArray(data_clean, info)
-		
-		# 提取注释信息
-		
-		bad_durations = np.sum(annot.duration)
-		
-
-
-		self.update_record('total_bad_duration', str(bad_durations))
-
 		return raw_clean
+	
 	def reference_data(self, raw):
 		return raw.set_eeg_reference(ref_channels='average', ch_type='eeg')
 	
-	def ica(self,raw):
+	def ica(self, raw):
 		# method='picard',
-
-		ica = ICA(method='infomax', fit_params=dict(extended=True),max_iter="auto", random_state=97)
+		
+		ica = ICA(method='infomax', fit_params=dict(extended=True), max_iter="auto", random_state=1019)
 		ica.fit(raw)
-		# scores = slope_score * focus_score * smoothness_score
-		muscle_idx, muscle_idx_scores = ica.find_bads_muscle(raw, threshold=0.9)
-
-		ecg_idx, ecg_scores = ica.find_bads_ecg(raw,ch_name='ECG1')
-		ic_labels = label_components(raw, ica, method="iclabel")
-		eog_idx = [
-			idx for idx, (prob, lbl) in enumerate(zip(ic_labels['y_pred_proba'], ic_labels['labels']))
-			if lbl in ["eye blink"] and prob > 0.9
-		]
-		print('*'*50)
-		# print('muscle_idx: ',len(muscle_idx))
-		print('ecg_idx: ',len(ecg_idx))
-		print('eog_idx: ',len(eog_idx))
-		combined_set =  set(ecg_idx) | set(eog_idx)
-		print('total:', len(combined_set))
-		print('*'*50)
+		if self.if_find_bads_muscle:
+			# scores = slope_score * focus_score * smoothness_score
+			muscle_idx, muscle_idx_scores = ica.find_bads_muscle(raw, threshold=0.9)
+		if self.if_find_bads_ecgs:
+			ecg_idx, ecg_scores = ica.find_bads_ecg(raw, ch_name='ECG1')
+		if self.if_find_bads_eogs:
+			ic_labels = label_components(raw, ica, method="iclabel")
+			eog_idx = [
+				idx for idx, (prob, lbl) in enumerate(zip(ic_labels['y_pred_proba'], ic_labels['labels']))
+				if lbl in ["eye blink"] and prob > self.threshold_pro_icalabel
+			]
+		combined_set = set()
+		if muscle_idx is not None:
+			combined_set.update(muscle_idx)
+		if ecg_idx is not None:
+			combined_set.update(ecg_idx)
+		if eog_idx is not None:
+			combined_set.update(eog_idx)
+		
+		# 去除重复索引并排除伪迹
 		ica.exclude = list(combined_set)
+		# 应用 ICA 去除伪迹成分
 		ica.apply(raw)
 		
-		self.update_record('muscle_idx: ', len(muscle_idx))
-		self.update_record('ecg_idx: ', len(ecg_idx))
-		self.update_record('eog_idx: ', len(eog_idx))
-		self.update_record('total_idx: ', len(combined_set))
+		# 更新记录
+		self.update_record('muscle_idx', len(muscle_idx) if muscle_idx is not None else 0)
+		self.update_record('ecg_idx', len(ecg_idx) if ecg_idx is not None else 0)
+		self.update_record('eog_idx', len(eog_idx) if eog_idx is not None else 0)
+		self.update_record('total_idx', len(combined_set))
 		
+		print('*' * 50)
+		print('muscle_idx: ', len(muscle_idx))
+		print('ecg_idx: ', len(ecg_idx))
+		print('eog_idx: ', len(eog_idx))
+		print('total:', len(combined_set))
+		print('*' * 50)
 		return raw
 	
-	def extract_data(self, raw, file_name):
+	def get_event_id(self, file_name):
 		if file_name.startswith('zero'):
 			event_ids = ["6021", "6022"]
 		if file_name.startswith('one'):
 			event_ids = ["6121", "6122"]
 		if file_name.startswith('two'):
 			event_ids = ["6221", "6222", '6223']
-		
+		return event_ids
+	
+	def extract_data(self, raw, file_name):
+		event_ids = self.get_event_id(file_name)
 		# 从注释中提取事件
 		events, event_id_map = mne.events_from_annotations(raw)
 		include_id = [event_id_map.get(id) for id in event_ids]
 		events_taril = mne.pick_events(events, include=include_id)
+		
 		if self.if_baseline:
-			epochs = mne.Epochs(raw, events_taril, tmin=-0.2, tmax=2.3,baseline=None, preload=True)
+			epochs = mne.Epochs(raw, events_taril, tmin=-0.2, tmax=2.3, baseline=(-0.2, 0), preload=True)
 		else:
 			epochs = mne.Epochs(raw, events_taril, tmin=0, tmax=2.5, baseline=None, preload=True)
 		
 		return epochs
 	
-	def run(self):
-
-		data_dir = self.get_data()
-		for key, value in data_dir.items():
-
-			file_dir = value
-			
-			if file_dir.split('\\')[-1].startswith("MATB"):
-				self.task = 'matb'
-			else:
-				self.task = 'back'
-			
-			raw = mne.io.read_raw_eeglab(file_dir, preload=True)
-
-			raw.set_channel_types({'ECG1': 'ecg'})
-			
-			dir = os.path.normpath(value).replace('set', 'fif').split(os.sep)
-			path = os.path.join('./Per_Process_3/', *dir[2:-2])
+	def process_file_path(self, file_dir):
+		raw = mne.io.read_raw_eeglab(file_dir, preload=True)
+		save_path = os.path.join("..", 'Per_Process', self.save_folder_name, os.path.dirname(file_dir)[3:].lstrip('\\'))
+		return raw, save_path
+	
+	def check_the_path(self, *paths):
+		for path in paths:
 			if not os.path.exists(path):
 				os.makedirs(path)
-			new_file_name = os.path.join(path, dir[-1])
-			
-			path_matlab = os.path.join(path, 'matlab_data')
-			if not os.path.exists(path_matlab):
-				os.makedirs(path_matlab)
-			new_file_name_matlab = os.path.join(path_matlab, dir[-1])
-			
-
-			# 去除Cz
-			raw = self.drop_channel(raw)
-			# 滤波1-80，50Hz陷波
-			raw = self.filter_data(raw)
-			# 重参考
-			raw = self.reference_data(raw)
-			# 去除坏导
-			raw = self.drop_bad_channels(raw)
-			# 去除坏段
-			if self.task == 'matb':
-				raw = self.drop_bad_period(raw)
-			
-			if self.task == 'back':
-				raw = self.extract_data(raw, dir[-1])
-
-
-			# 降采样
-			# raw.resample(sfreq=self.sfreq)
-
-			# ica去眼电与心电
-			raw = self.ica(raw)
-
-			raw_ecg = raw.copy().pick(picks = 'ecg')
-			raw_eeg = raw.copy().pick_types(eeg=True)
-			
-			raw_eeg.save(insert_before_nth_from_end(new_file_name, '_eeg', 4), overwrite=True)
-			raw_ecg.save(insert_before_nth_from_end(new_file_name, '_ecg', 4), overwrite=True)
-			
-
-			raw_eeg.export(insert_before_nth_from_end(new_file_name_matlab, '_eeg', 4).replace('fif', 'set'), overwrite=True)
-			raw_ecg.export(insert_before_nth_from_end(new_file_name_matlab, '_ecg', 4).replace('fif', 'set'), overwrite=True)
-			
+	
+	def process_raw(self, raw, save_path, file_name):
 		
+		raw.set_channel_types({'ECG1': 'ecg'})  # 设置通道类型
+		# 去除Cz
+		raw = self.drop_channel(raw)
+		self.append_string('drop_channel')
+		# 滤波1-80，50Hz陷波
+		raw = self.filter_data(raw)
+		self.append_string('filter')
+		# 重参考
+		raw = self.reference_data(raw)
+		self.append_string('reference')
+		# 去除坏导
+		raw = self.drop_bad_channels(raw)
+		self.append_string('drop_bad_channels')
+		# 去除坏段
+		if self.task == 'matb':
+			raw = self.drop_bad_period(raw)
+			self.append_string('drop_bad_period')
+		if self.task == 'back':
+			raw = self.extract_data(raw, file_name)
+			self.append_string('extract_data')
+		# 降采样
+		# raw.resample(sfreq=self.sfreq)
+		# self.append_string('resample')
+		# ica去眼电与心电
+		raw = self.ica(raw)
+		self.append_string('ica')
+		
+		raw_ecg = raw.copy().pick(picks='ecg')
+		raw_eeg = raw.copy().pick_types(eeg=True)
+		
+		save_path_matlab = os.path.join(save_path, 'matlab')
+		self.check_the_path(save_path, save_path.replace('eeg', 'ecg'), save_path_matlab,
+		                    save_path_matlab.replace('eeg', 'ecg'))
+		
+		raw_eeg.save(os.path.join(save_path, file_name + '_eeg.fif'), overwrite=True)
+		raw_ecg.save(os.path.join(save_path, file_name + '_ecg.fif').replace('eeg', 'ecg'), overwrite=True)
+		
+		raw_eeg.export(os.path.join(save_path_matlab, file_name + '_eeg.set'), overwrite=True)
+		raw_ecg.export(os.path.join(save_path_matlab, file_name + '_ecg.set').replace('eeg', 'ecg'), overwrite=True)
+		
+		if self.first_call:
+			# 只在第一次调用时执行
+			self.first_call = False
+	
+	def adjust_task(self, value):
+		file_dir = value
+		file_name = (value.split('\\')[-1]).split('.')[0]
+		# 判断任务类型
+		if file_name.startswith("MATB"):
+			self.task = 'matb'
+		elif file_name.endswith("BACK"):
+			self.task = 'back'
+		else:
+			raise ValueError("task type error")
+	
+	def run(self):
+		documentation_path = os.path.join("..", 'Per_Process', self.save_folder_name)
+		self.check_the_path(documentation_path)
+		
+		data_dir = self.get_dir()
+		for _, value in data_dir.items():
+			file_name = value.split('\\')[-1].split('.')[0]
+			self.adjust_task(file_name)
+			raw, save_path = self.process_file_path(value)
+			self.process_raw(raw, save_path, file_name)
+		
+		self.save_to_csv(documentation_path)
+		self.save_to_txt(documentation_path)
+
+
 process = Per_Process()
 process.run()
-process.save_to_csv()
